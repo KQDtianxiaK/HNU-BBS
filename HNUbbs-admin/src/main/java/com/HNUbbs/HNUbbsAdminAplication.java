4f7c506d4802b219295f1313376184aab49846f8
ClovertaTheTrilobita <cloverta@petalmail.com>
2024-07-27 17:34:37 +0800

    bbs first test just a framework

---
+++ HNUbbs-admin/src/main/java/com/HNUbbs/HNUbbsAdminAplication.java
@@ -0,0 +1,12 @@
+package com.HNUbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HNUbbsAdminAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HNUbbsAdminAplication.class, args);
+    }
+}
